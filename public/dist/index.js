defa44b4763d8adff29be57efea62b38bed6e63d
Luiz Phelipe <luizphsantana@gmail.com>
2024-10-03 21:20:00 -0300

    Started the implementation of account storage.

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    var accounts = [];
     // Open modal config
     $('#btn-config').on('click', function () {
         $('#modal-config').toggle(400);
@@ -17,7 +18,58 @@ $(document).ready(function () {
             $('body').css('overflow', 'auto');
         }
     });
+    var account = /** @class */ (function () {
+        function account(type, value, due, installments, paid) {
+            this.type = type;
+            this.value = value;
+            this.due = due;
+            this.installments = installments;
+            this.paid = paid;
+        }
+        return account;
+    }());
     // Mask input
     $('.input-money').mask('000.000.000.000.000,00', { reverse: true }); // Mask money
-    $('.input-date').mask('00/00'); // Mask date
+    $('.input-date').mask('00'); // Mask date
+    // Open modal add
+    $('#add-account-area').on('click', function () {
+        $('#modal-add').toggle(400);
+        $('body').css('overflow', 'hidden');
+    });
+    // close modal
+    $('#btn-close-add').on('click', function () {
+        $('#modal-add').toggle(400);
+        $('body').css('overflow', 'auto');
+    });
+    // Close modal on click in document
+    $(window).on('click', function (event) {
+        if (event.target === $('#modal-add').get(0)) {
+            $('#modal-add').toggle(400);
+            $('body').css('overflow', 'auto');
+        }
+    });
+    // Submit add account 
+    $('#btn-submit-add').on('click', function () {
+        if ($('#value').val() === '' || $('#venc').val() === '' || $('#parcelas').val() === '') {
+            alert('Preencha Todos os campos');
+        }
+        else {
+            var type = String($('#type').val());
+            var value = Number($('#value').val());
+            var venc = String($('#venc').val());
+            var parcelas = Number($('#parcelas').val());
+            var pago = void 0;
+            if ($('#pago').val() === 'true') {
+                pago = true;
+            }
+            else {
+                pago = false;
+            }
+            var newAccount = new account(type, value, venc, parcelas, pago);
+            accounts.push(newAccount);
+            alert(JSON.stringify(accounts, null, 2));
+        }
+    });
+    // Mask input
+    $('#venc').mask('00/00/0000');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+    const accounts: account[] = [];
     // Open modal config
     $('#btn-config').on('click', () => {
         $('#modal-config').toggle(400);
@@ -20,8 +21,76 @@ $(document).ready(() => {
         }
     })
 
+    interface Account {
+        type:string 
+        value:number
+        due:string // Due date
+        installments:number
+        paid:boolean
+    }
+
+    class account implements Account {
+        type: string;
+        value: number;
+        due: string;
+        installments: number;
+        paid: boolean;
+        constructor(type: string, value: number, due: string, installments: number, paid: boolean) {
+            this.type = type;
+            this.value = value;
+            this.due = due;
+            this.installments = installments;
+            this.paid = paid;
+        }
+    }
 
     // Mask input
     $('.input-money').mask('000.000.000.000.000,00', {reverse: true}); // Mask money
-    $('.input-date').mask('00/00'); // Mask date
+    $('.input-date').mask('00'); // Mask date
+
+
+    // Open modal add
+    $('#add-account-area').on('click', () => {
+        $('#modal-add').toggle(400);
+        $('body').css('overflow', 'hidden');
+    })
+
+    // close modal
+    $('#btn-close-add').on('click', () => {
+        $('#modal-add').toggle(400);
+        $('body').css('overflow', 'auto');
+    })
+
+    // Close modal on click in document
+    $(window).on('click', (event) => {
+        if (event.target === $('#modal-add').get(0)) {
+            $('#modal-add').toggle(400);
+            $('body').css('overflow', 'auto');
+        }
+    })
+
+    // Submit add account 
+    $('#btn-submit-add').on('click', () => {
+        if ($('#value').val() === '' || $('#venc').val() === '' || $('#parcelas').val() === '' ) {
+            alert('Preencha Todos os campos');
+        } else {
+            const type = String($('#type').val());
+            const value = Number($('#value').val());
+            const venc = String($('#venc').val());
+            const parcelas = Number($('#parcelas').val());
+            let pago:boolean;
+            if ($('#pago').val() === 'true') {
+                pago = true;
+            } else {
+                pago = false;
+            }
+            const newAccount = new account(type, value, venc, parcelas, pago);
+            accounts.push(newAccount);
+
+            alert(JSON.stringify(accounts, null, 2));
+        }
+    })
+
+    // Mask input
+    $('#venc').mask('00/00/0000')
 })
